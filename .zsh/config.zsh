0f7e9d00d5fc05ad6ce460f8f832fe477d49415d
jx11r <jx11r@hotmail.com>
2022-07-14 18:37:12 -0500

    zsh: set options

--- .zsh/config.zsh
+++ .zsh/config.zsh
@@ -1,8 +1,18 @@
 # History File
-HISTSIZE=10000
-SAVEHIST=10000
+HISTSIZE=5000
 HISTFILE=~/.zsh_history
-setopt appendhistory
+SAVEHIST=$HISTSIZE
+
+setopt EXTENDED_HISTORY
+setopt HIST_EXPIRE_DUPS_FIRST
+setopt HIST_IGNORE_DUPS
+setopt HIST_IGNORE_ALL_DUPS
+setopt HIST_IGNORE_SPACE
+setopt HIST_REDUCE_BLANKS
+setopt HIST_SAVE_NO_DUPS
+setopt HIST_VERIFY
+setopt INC_APPEND_HISTORY
+setopt SHARE_HISTORY
 
 # Autocomplete
 # zstyle ':completion:*' menu select=0
