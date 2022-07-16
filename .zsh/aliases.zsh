cad18b06e67822ac51523f39744da6fca808ceab
jx11r <jx11r@hotmail.com>
2022-07-16 00:27:06 -0500

    zsh: add globalias

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -1,18 +1,31 @@
 # --==[ Aliases ]==--
-alias ppd='popd'
-alias pd='pushd'
-alias cd..='cd ..'
-alias cl='clear'
-alias tar='tar -xf'
-alias dload='curl -O'
-alias grep='grep --color=auto'
-alias pacman='sudo pacman'
-alias mirror='sudo reflector --sort rate -l 5 --save /etc/pacman.d/mirrorlist'
-alias yay='yay --aur --devel --nocleanmenu --nodiffmenu --noeditmenu --removemake'
+autoload -Uz compinit && compinit
 
-# Xephyr
-alias Xone='Xephyr -screen 720x480 :1'
-alias Xtwo='Xephyr -screen 640x480 -screen 640x480 +xinerama :1'
+globalias() {
+  local word=${${(Az)LBUFFER}[-1]}
+
+  [[ $GLOBALIAS_FILTER_VALUES[(Ie)$word] -eq 0 ]] && {
+    zle _expand_alias
+    zle expand-word
+  }
+
+  zle self-insert
+}
+
+zle -N globalias
+bindkey " " globalias
+bindkey -M isearch " " magic-space
+
+# System
+alias reflector='sudo reflector --sort rate -l 5 --save /etc/pacman.d/mirrorlist'
+alias pacman='sudo pacman'
+alias yay='yay --devel'
+alias dload='curl -O'
+alias cl='clear'
+alias cd..='cd ..'
+alias tar='tar -xf'
+alias grep='grep --color=auto'
+alias -g L='| less'
 
 # Files
 alias ls='exa --group-directories-first'
@@ -21,21 +34,31 @@ alias tree='exa -T'
 alias cat='bat'
 alias vi='nvim'
 
+# Xephyr
+alias Xone='Xephyr -screen 720x480 :1'
+alias Xtwo='Xephyr -screen 640x480 -screen 640x480 +xinerama :1'
+
 # Git
-alias gpsh='git push'
-alias gpll='git pull'
-alias gl='git log --oneline'
-alias gs='git status'
-alias gr='git restore'
-alias gd='git diff'
 alias ga='git add'
 alias gb='git branch'
+alias gc='git clone'
+alias gd='git diff'
+alias gf='git fetch'
+alias gl='git log --oneline'
+alias gm='git merge'
+alias gr='git restore'
+alias gs='git status'
 alias gco='git checkout'
-alias gsw='git switch'
+alias grb='git rebase'
+alias grm='git remote'
+alias grs='git reset'
 alias gst='git stash'
+alias gsw='git switch'
 alias gcm='git commit -m'
 alias gcma='git commit -a -m'
+alias gpll='git pull'
+alias gpsh='git push'
 
-# Info
-alias fetch='neofetch'
-alias usage='du -sh'
+GLOBALIAS_FILTER_VALUES=(
+  pacman yay dload cl cd.. tar grep ls ll tree cat vi
+)
