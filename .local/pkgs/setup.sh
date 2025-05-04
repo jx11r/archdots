3c58a2a9a5e6d1f1fcabf13366dc5e6ec5185250
jx11r <jx11r@hotmail.com>
2025-05-04 17:05:52 -0600

    pkgs: improve setup

--- .local/pkgs/setup.sh
+++ .local/pkgs/setup.sh
@@ -8,11 +8,6 @@ green="\033[1;32m"
 blue="\033[1;34m"
 error="${red}error:${reset}"
 
-if [ $EUID -ne 0 ]; then
-  echo -e "${error} ${green}setup.sh${reset} must be run as root"
-  exit 1
-fi
-
 if [ ! -f /tmp/.pkgs_installed ]; then
   echo -e "${error} ${green}install${reset} has not been executed"
   exit 1
@@ -20,12 +15,22 @@ fi
 
 PAM_FILE="/etc/pam.d/login"
 if ! grep -q "pam_gnome_keyring.so" "$PAM_FILE"; then
-  cp $PAM_FILE "${PAM_FILE}.old"
-  sed -i "6 i auth       optional     pam_gnome_keyring.so" $PAM_FILE
-  echo "session    optional     pam_gnome_keyring.so auto_start" >> $PAM_FILE
+  sudo cp $PAM_FILE "${PAM_FILE}.old"
+  sudo sed -i "6 i auth       optional     pam_gnome_keyring.so" $PAM_FILE
+  sudo echo "session    optional     pam_gnome_keyring.so auto_start" >> $PAM_FILE
   echo -e " ${blue}${PAM_FILE}${reset} updated"
 else
   echo -e " ${blue}${PAM_FILE}${reset} already configured"
 fi
+
+echo  " cloning qtile config..."
+git clone https://github.com/jx11r/qtile.git ~/.config/qtile
+
+echo " building bat cache..."
+bat cache --build
+
+echo " changing default shell to zsh..."
+sudo chsh -s /usr/bin/zsh
+chsh -s /usr/bin/zsh
 
 echo " post-install setup completed"
