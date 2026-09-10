86f7cb28cbbc537e5f58514827b44681388f8787
jx11r <git@jx11r.dev>
2026-09-09 22:16:57 -0600

    awesome: render taglist with custom icons (WIP)

--- .config/awesome/widgets/taglist.lua
+++ .config/awesome/widgets/taglist.lua
@@ -2,27 +2,49 @@ local awful = require("awful")
 local wibox = require("wibox")
 local beautiful = require("beautiful")
 
-local icons = {
+local status_icons = {
   active   = "",
   empty    = "",
   occupied = "",
 }
 
-local function update_tag(self, tag, index, colors, inactive)
+local function by_state(self, tag, index, colors, icons, inactive)
   local text = self:get_children_by_id("tag_icon")[1]
-
   local color = colors[index] or beautiful.fg_normal
   local icon
 
-  if #tag:clients() > 0 and tag.urgent then
-    icon = icons.occupied
+  if tag.urgent then
+    icon = status_icons.occupied
     color = beautiful.bg_urgent
   elseif tag.selected then
-    icon = icons.active
+    icon = status_icons.active
   elseif #tag:clients() > 0 then
-    icon = icons.occupied
+    icon = status_icons.occupied
   else
-    icon = icons.empty
+    icon = status_icons.empty
+    color = inactive
+  end
+
+  text:set_markup(
+    string.format(
+      "<span foreground='%s'>%s</span>",
+      color,
+      icon
+    )
+  )
+end
+
+-- TODO: improve when tag is selected
+local function by_icon(self, tag, index, colors, icons, inactive)
+  local text = self:get_children_by_id("tag_icon")[1]
+  local color = colors[index] or beautiful.fg_normal
+  local icon = icons[index] or tag.name
+
+  if tag.urgent then
+    color = beautiful.bg_urgent
+  elseif tag.selected and #tag:clients() == 0 then
+    color = inactive
+  elseif #tag:clients() == 0 then
     color = inactive
   end
 
@@ -39,14 +61,20 @@ return function(args)
   args = args or {}
 
   local colors = args.colors or {}
-  local inactive = args.inactive or beautiful.fg_normal
   local font = args.font or beautiful.font
+  local inactive = args.inactive or beautiful.fg_normal
+  local spacing = args.spacing or 8
+  local icons = args.icons or false
+  local render = icons and by_icon or by_state
 
   args.colors = nil
+  args.font = nil
   args.inactive = nil
+  args.spacing = nil
+  args.icons = nil
 
   args.layout = args.layout or {
-    spacing = 8,
+    spacing = spacing,
     layout = wibox.layout.fixed.horizontal,
   }
 
@@ -61,12 +89,12 @@ return function(args)
 
     widget = wibox.container.background,
 
-    create_callback = function(self, tag, index, objects)
-      update_tag(self, tag, index, colors, inactive)
+    create_callback = function(self, tag, index)
+      render(self, tag, index, colors, icons, inactive)
     end,
 
-    update_callback = function(self, tag, index, objects)
-      update_tag(self, tag, index, colors, inactive)
+    update_callback = function(self, tag, index)
+      render(self, tag, index, colors, icons, inactive)
     end,
   }
 
